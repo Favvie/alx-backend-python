bbd04ca3ab3055115e2c6c032818dc00973926cc
Favvie <favourprecious3@gmail.com>
2023-01-06 01:19:16 +0100

    [fix] add function docstring

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """floor function module"""
-
 import math
 
 
 def floor(n: float) -> int:
+    """a function that takes in a float and returns its floored value"""
     return math.floor(n)
