9dcebde898dbc1462494f6c429980460638002f6
Favvie <favourprecious3@gmail.com>
2023-01-06 01:15:52 +0100

    [fix] pycodestyle update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,5 +5,5 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """a function that takes in complex types"""
-    
+
     return (k, float(v ** 2))
