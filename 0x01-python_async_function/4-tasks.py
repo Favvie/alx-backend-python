79a33571d8eed2a02edfed43e7f6332a8db6367c
favvie <favourabangwu@gmail.com>
2023-04-04 05:40:36 +0100

    [fix] add function docstring

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,6 +8,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return a list of delay times"""
     tasks = []
     for _ in range(n):
         task = task_wait_random(max_delay)
