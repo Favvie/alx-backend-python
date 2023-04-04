e053182a21342e8bcc22ce3d87b1f0e712517841
favvie <favourabangwu@gmail.com>
2023-04-04 16:41:02 +0100

    [fix] add docstring for async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """ a coroutine that using async comprehension"""
     values = [i async for i in async_generator()]
     return values
