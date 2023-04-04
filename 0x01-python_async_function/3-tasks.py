cb319bbebf90b7d5cbf374efd01b40f3bd82c188
favvie <favourabangwu@gmail.com>
2023-04-04 04:45:53 +0100

    [fix] error in task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,5 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """a non-async func that return asyncio.Task"""
-    task = asyncio.create_task(wait_random(5))
+    task = asyncio.create_task(wait_random(max_delay))
     return task
