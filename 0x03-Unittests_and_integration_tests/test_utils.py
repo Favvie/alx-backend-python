10630be7103c90bdb3bc18b8edb01c57c882da89
favvie <favourabangwu@gmail.com>
2023-04-22 14:32:05 +0100

    task 0: test access nested map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,34 @@
+#!/usr.bin/env python3
+"""test utils.py file"""
+
+
+import unittest
+from parameterized import parameterized
+access_nested_map = __import__('utils').access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class for testing accessed nested map function
+    Inherits from unittest.TestCase class
+    Parameters
+    ----------
+    unittest.TestCase
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test the access nested map with key path.
+        Parameters
+        ----------
+        nested_map: Mapping
+        A nested map
+        path: Sequence
+        a sequence of key representing a path to the value
+        expected
+        expected result
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
